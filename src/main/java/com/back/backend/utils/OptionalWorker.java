e32ba8728337de325bf203ad3fb902afdd2adad2
Aleksandr Volkov3 <aleksandr_volkov3@epam.com>
2022-07-19 20:16:48 +0300

    fix: remove service annotation from OptionalWorker

--- src/main/java/com/back/backend/utils/OptionalWorker.java
+++ src/main/java/com/back/backend/utils/OptionalWorker.java
@@ -1,10 +1,8 @@
 package com.back.backend.utils;
 
 import com.back.backend.exceptions.OptionalNotFoundException;
-import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-@Service
 public class OptionalWorker {
     public static void checkOptional(Optional optional) throws OptionalNotFoundException {
         if (optional.isEmpty()) {
